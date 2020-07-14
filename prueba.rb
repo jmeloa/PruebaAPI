4689ee50aedcf5d6eb978322cd5467cd1bda4777
jmelo <jmeloa@gmail.com>
2020-07-14 19:33:29 -0400

    Version 3. Optimizo con each

--- prueba.rb
+++ prueba.rb
@@ -16,17 +16,24 @@ def build_web_page(data)
     #new_hash={}
     i=0
     html=""
-    
-    for i in (0..24) 
-        html=  "\n>\t<li><img src=\"#{data["photos"][i]["img_src"]}\"></li>"  + html
-    end    
-    
-   encabezado="<html>\n<head>\n</head>\n<body>\n<ul>"
-   footer="\n</ul>\n</body>\n</html>"
-   File.write('output.html', encabezado + html+footer)
+    new_hash={}
+    new_hash=data["photos"]
+    new_hash.each do |k,v| 
+        html=  "\n>\t<li><img src=\"#{k["img_src"]}\"></li>"  + html
+    end
+    encabezado="<html>\n<head>\n</head>\n<body>\n<ul>"
+    footer="\n</ul>\n</body>\n</html>"
+    File.write('output.html', encabezado + html+footer)
 end    
 
+def photos_count(data)
+    
+end
 
-data = request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=OOEYkgTiuw6p6GNNQCuLYei8BFmxXYBL3y06wo8W') # Limitamos los resultados a 10
+web="https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key="
+key="OOEYkgTiuw6p6GNNQCuLYei8BFmxXYBL3y06wo8W"
+data = request(web+key) # Limitamos los resultados a 10
 
 build_web_page (data)
+photos_count (data)
+
